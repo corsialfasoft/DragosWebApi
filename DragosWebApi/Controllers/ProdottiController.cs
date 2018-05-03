f1ca4ef82dd6b60cee6a9571f700a8c202b70778
TandyD <brinzila30@gmail.com>
2018-05-03 10:36:52 +0200

    ok per carmen

---
+++ DragosWebApi/Controllers/ProdottiController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DragosWebApi.Models;
+
+namespace DragosWebApi.Controllers {
+    public class ProdottiController : ApiController {
+        
+        DomainModel dm = new DomainModel();
+        // GET api/<controller>
+        public IEnumerable<string> Get() {
+           
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id) {
+            
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value) {
+            
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id,[FromBody]string value) {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id) {
+           
+        }
+    }
+}
---
+++ DragosWebApi/Models/DomainModelcs.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DragosWebApi.Models {
+	interface IDomainModel{
+		Prodotto Search(int id);
+		List<Prodotto> Search(string des);
+		void SpedisciOrdine(List<Prodotto> prodotti);
+        void Del(int id);
+        void Add(Prodotto nuovo);
+
+	}
+
+	public class Prodotto {
+		public int Id{ get;set;}
+		public string Descrizione{ get;set;}
+		public int Giacenza{ get;set;}
+		public int QtaOrdine{ get;set;}
+        public override bool Equals(object obj) {
+            return this.Id==((Prodotto)obj).Id;
+        }
+    }
+
+	public class DomainModel : IDomainModel {
+		RICHIESTEEntities db = null;
+		public Prodotto Search(int id) {
+			using(db= new RICHIESTEEntities()){
+				ProdottiSet prodotto = db.ProdottiSet.Find(id);
+				if(prodotto!=null){
+					return new Prodotto{ Id=prodotto.id,Descrizione=prodotto.descrizione,Giacenza=prodotto.giacenza};
+				}
+				return null;
+			}
+		}
+
+		public List<Prodotto> Search(string des) {
+			using (db = new RICHIESTEEntities()) {
+				var query = from pro in db.ProdottiSet
+							where pro.descrizione.Contains(des)
+							select pro;
+				List<Prodotto> prodotti = new List<Prodotto>();
+ 				foreach(ProdottiSet prodotto in query)
+					prodotti.Add(new Prodotto { Id = prodotto.id, Descrizione = prodotto.descrizione, Giacenza = prodotto.giacenza });
+				return prodotti;
+			}
+		}
+
+		public void SpedisciOrdine(List<Prodotto> prodotti) {
+			using (db = new RICHIESTEEntities()) {
+				OrdiniSet richiesta = new OrdiniSet{dataOrdine=DateTime.Now};
+				db.OrdiniSet.Add(richiesta);
+				foreach(Prodotto prodotto in prodotti){
+					ProdottiSet ps = db.ProdottiSet.Find(prodotto.Id);
+					if(ps!=null){ 
+						OridiniProdotti rp =new OridiniProdotti{OrdiniSet = richiesta, ProdottiSet = ps, quantita= prodotto.QtaOrdine};
+						db.OridiniProdotti.Add(rp);
+						db.SaveChanges();
+					}
+				}
+
+			}
+		}
+        public void Del(int id){ 
+            using (db = new RICHIESTEEntities()) {
+			    ProdottiSet elimina = db.ProdottiSet.Find(id);
+			    db.ProdottiSet.Remove(elimina);
+                db.SaveChanges();
+			    }   
+        }   
+        public void Add(Prodotto nuovo){
+            using (db = new RICHIESTEEntities()) {
+			    ProdottiSet aggiungi = new ProdottiSet{descrizione=nuovo.Descrizione, giacenza=nuovo.Giacenza};
+                db.ProdottiSet.Add(aggiungi);
+                db.SaveChanges();
+			    }   
+            }
+	}
+}
